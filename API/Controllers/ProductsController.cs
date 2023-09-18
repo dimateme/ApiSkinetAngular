87409304f14a6e5c7b2248cedd7488dfd1aaf496
dimateme <jipidimatem@gmail.com>
2023-09-18 09:11:03 -0400

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //https://localhost:5001/api/products
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Method to get all products from the database
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products =await _context.Products.ToListAsync();
+            return products;
+        }
+        /// <summary>
+        /// Method to get a single product from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>>GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext: DbContext
+    {
+        /// <summary>
+        /// Constructor for StoreContext
+        /// </summary>
+        /// <param name="options"></param>
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        /// <summary>
+        /// Creates a table in the database called Products
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
+    }
+}
